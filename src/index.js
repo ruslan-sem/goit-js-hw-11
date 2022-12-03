3f64616c3ffd9a20b16a36ee399f9af863d4a240
Ruslan Samoylenko <ruslan.sem@gmail.com>
2022-12-03 15:59:37 +0200

    skel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import axios from 'axios';
+import { fetchPhotos } from './js/fetchPhotos';
+
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const btn = document.querySelector('.load-more');
+let currentpage = 1;
+let currentquery = '';
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  currentpage = 1;
+  gallery.innerHTML = '';
+  currentquery = event.currentTarget.searchQuery.value.trim();
+  if (currentquery === '') {
+    return;
+  }
+});
---
+++ src/js/fetchPhotos.js
@@ -0,0 +1,21 @@
+export async function fetchPhotos(query, page) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const KEY = '31621045-27b3574e35e935623860605ff';
+  const searchParams = {
+    params: {
+      key: KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page,
+    },
+  };
+
+  try {
+    return await axios.get(BASE_URL, searchParams);
+  } catch (error) {
+    console.error(error);
+  }
+}
